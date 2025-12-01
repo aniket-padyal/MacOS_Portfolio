d3051fe31cc9ceba1fa3c528c11a4c8cf02aa3b1
aniket-padyal <padyal10aniket@gmail.com>
2025-12-01 20:20:58 +0530

    implemented few suggestion and typos

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -23,7 +23,7 @@ const renderText = (text, className, baseWeight = 400) => {
 
 // Function to set up hover effect on text
 const setupTextHover = (container, type) => {
-  if (!container) return;
+  if (!container) return () => {};
 
   const letters = container.querySelectorAll("span");
   const { min, max, default: base } = FONT_WEIGHTS[type];
